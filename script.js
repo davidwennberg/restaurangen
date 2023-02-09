5f4c35d4577f7952e5cb9d52b7026c6b195df797
davidwennberg <david.wennberg@student.tabyenskilda.se>
2023-02-09 11:47:20 +0100

    v.5

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
+/*Öppnar sidonav*/
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
-    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+    document.body.style.backgroundColor = "rgb(0, 31, 21)";
   }
+  /*Stänger sidonav*/
   function closeNav() {
     document.getElementById("mySidenav").style.width ="0";
-    document.body.style.backgroundColor = "white";
+    document.body.style.backgroundColor = "rgb(0, 61, 41)";
   }
